df95b6399edc2dc408052cb1cbd6503fe9626d97
dim_qa <dmitrjp@gmail.com>
2025-03-15 18:32:30 +0300

    Фикс имя пользователя (Что автоматизировать и когда выполнять автотесты)

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -11,7 +11,7 @@ def test_post_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'dm_qa_018'
+    login = 'dm_qa_100'
     password = '987654321'
     email = f'{login}@mail.ru'
     json_data = {
