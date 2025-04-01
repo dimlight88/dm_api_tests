b83d310a7ccb52cbac6e02b7c5863abf43dec6cd
dim_qa <dmitrjp@gmail.com>
2025-04-01 15:29:19 +0300

    Добавил ДЗ по Прокси разделу (забыл добавить в ветку прокси, добавляю в ветку Фасадов, далее сюда же добавлю дз по Фасадам)

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -4,16 +4,32 @@ import time
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
+import structlog
+
+structlog.configure(
+    processors=[
+        structlog.processors.JSONRenderer(
+            indent=4,
+            ensure_ascii=True,
+            # sort_keys=True
+        )
+    ]
+)
 
 
 def test_put_v1_account_email():
     # Регистрация пользователя
 
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'dm_qa_put126'
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mailhog_api = MailhogApi(configuration=mailhog_configuration)
+
+    login = 'dm_qa_put128'
     password = '987654321'
     email = f'{login}@mail.ru'
     json_data = {
@@ -24,16 +40,12 @@ def test_put_v1_account_email():
 
     response = account_api.post_v1_account(json_data=json_data)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
 
     # Получаем письма из почтового сервера для активации аккаунта
 
     response = mailhog_api.get_api_v2_messages()
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Письма не были получены"
 
     # Из писем извлекаем токен активации регистрации
@@ -46,8 +58,6 @@ def test_put_v1_account_email():
 
     response = account_api.put_v1_account_token(token=token)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не был активирован"
 
     # Авторизация
@@ -60,8 +70,6 @@ def test_put_v1_account_email():
 
     response = login_api.post_v1_account_login(json_data=json_data)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
 
     # Меняем email
@@ -74,8 +82,6 @@ def test_put_v1_account_email():
     }
     response = account_api.put_v1_account_email(json_data=json_data)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
 
     # Пытаемся авторизоваться – получаем 403
@@ -87,16 +93,12 @@ def test_put_v1_account_email():
     }
     response = login_api.post_v1_account_login(json_data=json_data)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 403, "Пользователь не смог авторизоваться"
 
     # Смотрим еще раз письма
 
     response = mailhog_api.get_api_v2_messages()
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Письма не были получены"
 
     # Из писем извлекаем токен подтверждения смены email
@@ -109,8 +111,6 @@ def test_put_v1_account_email():
 
     response = account_api.put_v1_account_token(token=token)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не был активирован"
 
     # Логинимся
@@ -122,8 +122,6 @@ def test_put_v1_account_email():
     }
     response = login_api.post_v1_account_login(json_data=json_data)
 
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200, "Пользователь не смог авторизоваться"
 
 
